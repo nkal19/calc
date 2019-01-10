d6bff568fb4e8f6c245cd49f7a06c919f911b2bd
Nitin Kalaiya <nkalaiya@mdsol.com>
2019-01-10 20:29:26 +0000

    Add spring boot skeleton

---
+++ src/main/java/com/example/calc/CalcApplication.java
@@ -0,0 +1,14 @@
+package com.example.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/calc/CalcApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.calc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
