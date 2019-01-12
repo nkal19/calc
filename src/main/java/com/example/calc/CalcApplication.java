c4a0f507112f0f57e15b50c0c9378325771aa8d3
Nitin Kalaiya <nkalaiya@mdsol.com>
2019-01-12 12:21:25 +0000

    added checkstyle module

--- src/main/java/com/example/calc/CalcApplication.java
+++ src/main/java/com/example/calc/CalcApplication.java
@@ -3,6 +3,9 @@ package com.example.calc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Spring Application.
+ */
 @SpringBootApplication
 public class CalcApplication {
 
--- src/main/java/com/example/calc/Calculator.java
+++ src/main/java/com/example/calc/Calculator.java
@@ -2,6 +2,9 @@ package com.example.calc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Main Spring Application.
+ */
 @Service
 public class Calculator {
 int sum(int a, int b){
